e0fd88c6af6af9a950b28c64cc8931223ed89d70
kjirou <kjirou.web@gmail.com>
2016-02-06 03:06:18 +0900

    Add alterTransition

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 'use strict';
 
+const omit = require('lodash.omit');
+
 const FlipBook = require('./lib/react-flip-book');
 
 
 /*
- * Generate transitions for react-flip-book
+ * Generate transition from prop-diffs
  * @param {Object} defaultProps
  * @param {Array<Object>} transitionDiffs
  * @return {Array<Object>}
  */
-const generateTransitions = (defaultProps, transitionDiffs) => {
+const generateTransition = (defaultProps, transitionDiffs) => {
   let currentProps = Object.assign({}, defaultProps);
   return transitionDiffs.map(diff => {
     const duration = diff.duration || 0;
@@ -26,7 +28,124 @@ const generateTransitions = (defaultProps, transitionDiffs) => {
   });
 };
 
+/*
+ * Alter a transition by any turning points containing keyframes
+ *
+ * Example 1:
+ * ```
+ * transition:
+ *   [
+ *     { duration:  50, icon: 'slash_1', hp: 3 },
+ *     { duration: 100, icon: 'slash_2', hp: 3 },
+ *     { duration:  50, icon: 'slash_1', hp: 3 },
+ *   ]
+ * alterations:
+ *   [
+ *     { keyframe: 75, hp: 2 },
+ *   ]
+ * result:
+ *   [
+ *     { duration: 50, icon: 'slash_1', hp: 3 },
+ *     { duration: 25, icon: 'slash_2', hp: 3 },
+ *     { duration: 75, icon: 'slash_2', hp: 2 },
+ *     { duration: 50, icon: 'slash_1', hp: 2 },
+ *   ]
+ * ```
+ *
+ * Example 2:
+ * ```
+ * transition:
+ *   [
+ *     { duration:  50, icon: 'slash_1', hp: 3 },
+ *     { duration: 100, icon: 'slash_2', hp: 1 },
+ *     { duration:  50, icon: 'slash_1', hp: 1 },
+ *   ]
+ * alterations:
+ *   [
+ *     { keyframe: 75, hp: 2 },
+ *   ]
+ * result:
+ *   [
+ *     { duration: 50, icon: 'slash_1', hp: 3 },
+ *     { duration: 25, icon: 'slash_2', hp: 1 },
+ *     { duration: 75, icon: 'slash_2', hp: 2 },  // Ignore transition's hp after alteration applying
+ *     { duration: 50, icon: 'slash_1', hp: 2 },
+ *   ]
+ * ```
+ *
+ * @return {Array<Object>}
+ */
+const alterTransition = (transition, alterations) => {
+  // TODO: assert undefined keyframes and durations
+  const keyframeObjects = [];
+  const appendKeyframeObject = (keyframe, props, alteration) => {
+    keyframeObjects.push({
+      keyframe,
+      props,
+      alteration,
+    });
+  };
 
-FlipBook.generateTransitions = generateTransitions;
+  let endKeyframe = 0;
+  transition.forEach(props => {
+    appendKeyframeObject(endKeyframe, props, null);
+    endKeyframe += props.duration;
+  });
 
+  // ensure that the last keyframe is props
+  appendKeyframeObject(endKeyframe, null, null);
+
+  alterations
+    .filter(alteration => alteration.keyframe < endKeyframe)
+    .forEach(alteration => {
+      const keyframeObjectOnTheSameFrame = keyframeObjects.filter(keyframeObject => {
+        return keyframeObject.keyframe === alteration.keyframe;
+      })[0];
+      if (keyframeObjectOnTheSameFrame) {
+        keyframeObjectOnTheSameFrame.alteration = alteration;
+      } else {
+        appendKeyframeObject(alteration.keyframe, null, alteration);
+      }
+    })
+  ;
+
+  keyframeObjects.sort((a, b) => a.keyframe - b.keyframe);
+
+  const lastProps = {};
+  const mergedAlternation = {};
+  return keyframeObjects
+    .map((keyframeObject, index) => {
+      const nextKeyframeObject = keyframeObjects[index + 1] || null;
+      if (!nextKeyframeObject) {
+        return null;
+      }
+      const duration = nextKeyframeObject.keyframe - keyframeObject.keyframe;
+
+      if (keyframeObject.alteration) {
+        Object.assign(mergedAlternation, omit(keyframeObject.alteration, 'keyframe'));
+      }
+
+      const newProps = {};
+      if (keyframeObject.props) {
+        Object.assign(newProps, keyframeObject.props, mergedAlternation, { duration });
+      } else {
+        Object.assign(newProps, lastProps, mergedAlternation, { duration });
+      }
+
+      Object.assign(lastProps, newProps);
+
+      return newProps;
+    })
+    .filter(keyframeObject => keyframeObject !== null)
+  ;
+};
+
+const totalDurations = (...transitionList) => {
+};
+
+
+Object.assign(FlipBook, {
+  alterTransition,
+  generateTransition,
+});
 module.exports = FlipBook;
--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@
 const assert = require('assert');
 
 const FlipBook = require('../src').default;
-const generateTransitions = require('../src').generateTransitions;
+const generateTransition = require('../src').generateTransition;
+const alterTransition = require('../src').alterTransition;
 
 
 describe('react-flip-book', () => {
@@ -15,11 +16,11 @@ describe('react-flip-book', () => {
     });
   });
 
-  describe('generateTransitions', () => {
+  describe('generateTransition', () => {
 
     it('simple use', () => {
       assert.deepEqual(
-        generateTransitions(
+        generateTransition(
           {
             x: 1,
             y: 2,
@@ -49,7 +50,7 @@ describe('react-flip-book', () => {
 
     it('should set duration', () => {
       assert.deepEqual(
-        generateTransitions(
+        generateTransition(
           {
             x: 1,
             y: 2,
@@ -82,7 +83,7 @@ describe('react-flip-book', () => {
 
     it('complex use', () => {
       assert.deepEqual(
-        generateTransitions(
+        generateTransition(
           {
             x: 1,
             y: 2,
@@ -125,6 +126,123 @@ describe('react-flip-book', () => {
           },
         ]
       );
+    });
+  });
+
+
+  describe('alterTransition', () => {
+
+    it('should be', () => {
+      const transition = [
+        {
+          duration: 50,
+          x: 1,
+          y: 10,
+        },
+        {
+          duration: 100,
+          x: 2,
+          y: 20,
+        },
+      ];
+      const alterations = [
+        {
+          keyframe: 75,
+          y: 21,
+        },
+      ];
+
+      const actual = alterTransition(transition, alterations);
+      assert.deepEqual(actual, [
+        {
+          duration: 50,
+          x: 1,
+          y: 10,
+        },
+        {
+          duration: 25,
+          x: 2,
+          y: 20,
+        },
+        {
+          duration: 75,
+          x: 2,
+          y: 21,
+        },
+      ]);
+    });
+
+    it('alterations are continuous at the tail', () => {
+      const transition = [
+        {
+          duration: 100,
+          x: 1,
+          y: 2,
+        },
+      ];
+      const alterations = [
+        {
+          keyframe: 25,
+          y: 3,
+        },
+        {
+          keyframe: 75,
+          y: 4,
+        },
+      ];
+
+      const actual = alterTransition(transition, alterations);
+      assert.deepEqual(actual, [
+        {
+          duration: 25,
+          x: 1,
+          y: 2,
+        },
+        {
+          duration: 50,
+          x: 1,
+          y: 3,
+        },
+        {
+          duration: 25,
+          x: 1,
+          y: 4,
+        },
+      ]);
+    });
+
+    it('should ignore keyframes outside the durations', () => {
+      const transition = [
+        {
+          duration: 100,
+          x: 1,
+          y: 2,
+        },
+      ];
+      const alterations = [
+        {
+          keyframe: 99,
+          y: 3,
+        },
+        {
+          keyframe: 100,
+          y: 4,
+        },
+      ];
+
+      const actual = alterTransition(transition, alterations);
+      assert.deepEqual(actual, [
+        {
+          duration: 99,
+          x: 1,
+          y: 2,
+        },
+        {
+          duration: 1,
+          x: 1,
+          y: 3,
+        },
+      ]);
     });
   });
 });
